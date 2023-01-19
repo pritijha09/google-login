f8c095ab094706d6bf967965afa3a2706489350f
PPKUMA <priti_kumari@us.crawco.com>
2023-01-19 07:42:32 -0500

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './auth-guard.service';
 import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
@@ -9,7 +10,7 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot([
     {path: '', redirectTo:'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
-    {path: 'mainpage', component: MainPageComponent}, // only accessible if authorised canActivate: [AuthGuardService]
+    {path: 'mainpage', component: MainPageComponent, canActivate: [AuthGuardService]}, // only accessible if authorised
     {path: '**', component: LoginComponent}
   ])],
   exports: [RouterModule]
